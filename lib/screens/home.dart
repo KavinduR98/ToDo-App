91983e53acfeb3237295867741767210f686a8c5
@kavinduushan07 <kavinduushan07@gmail.com>
2023-02-19 23:56:54 +0530

    Create custom AppBar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -6,10 +7,35 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('ToDo App')),
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
       body: Container(
-        child: Text('This is home screen.'),
+        child: Column(
+          children: [Container()],
+        ),
       ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0.0,
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Icon(
+          Icons.menu,
+          color: tdBlack,
+          size: 30.0,
+        ),
+        Container(
+          height: 40.0,
+          width: 40.0,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.asset("assets/images/avatar.jpeg"),
+          ),
+        )
+      ]),
     );
   }
 }
